c7697d627f99955af4f40738b59d3b645c973441
Sharu <jsrshark110@gmail.com>
2022-03-15 13:06:52 -0700

    update config

--- scripts/run.js
+++ scripts/run.js
@@ -5,6 +5,11 @@
 // Runtime Environment's members available in the global scope.
 const hre = require("hardhat");
 
+const mint = async (contract, addr, description) => {
+  let txn = await contract.mint(addr, description);
+  await txn.wait();
+}
+
 async function main() {
   // Hardhat always runs the compile task when running scripts with its command
   // line interface.
@@ -15,11 +20,19 @@ async function main() {
 
   // We get the contract to deploy
   const MerkleNft = await hre.ethers.getContractFactory("MerkleNftV2");
+  console.log("Start deploying...");
   const nft = await MerkleNft.deploy();
 
   await nft.deployed();
+  console.log("Merkle nft v2 deployed to:", nft.address);  
 
-  console.log("Merkle nft v2 deployed to:", nft.address);
+  const [owner] = await hre.ethers.getSigners();
+
+  console.log("Minting nfts");
+  // Mint NFTs
+  await mint(nft, owner.address, "nft 1");
+  await mint(nft, owner.address, "nft 2");
+  await mint(nft, owner.address, "nft 3");
 }
 
 // We recommend this pattern to be able to use async/await everywhere
